c00c3d170e51adb9262784fb6a61c007b0eddcea
Somen Pradhan <somenpradhan135@gmail.com>
2025-05-25 21:00:00 +0530

    Delete Move_All_Zeroes_to_End.py

--- Move_All_Zeroes_to_End.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def pushZerosToEnd(self, arr):
-        n = len(arr)
-        nonZeroIndex = 0
-
-        for i in range(n):
-            if arr[i] != 0:
-                arr[nonZeroIndex], arr[i] = arr[i], arr[nonZeroIndex]
-                nonZeroIndex += 1
